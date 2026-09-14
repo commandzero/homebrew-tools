62f12f68943641d564ab49228e2b6a340c5a68e5
Ryan Eno <ryan.eno@elastic.co>
2026-09-13 17:14:07 -0700

    chore(tless): update formula to 0.1.2 (#2)

--- Formula/tless.rb
+++ Formula/tless.rb
@@ -1,28 +1,28 @@
 class Tless < Formula
   desc "Terminal viewer for JSON, YAML, and TOON"
   homepage "https://github.com/commandzero/tless"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/CommandZero/tless/releases/download/v0.1.1/tless-v0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "f0b8cb22a6398b909d41c39d217af9ddd4d67289447c5709030133476722ad44"
+      url "https://github.com/CommandZero/tless/releases/download/v0.1.2/tless-v0.1.2-aarch64-apple-darwin.tar.gz"
+      sha256 "20d16b127014ed6889f22a38855fd6b4765cf845d6b735c96ec3e00082e6d11d"
     end
     on_intel do
-      url "https://github.com/CommandZero/tless/releases/download/v0.1.1/tless-v0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "8a7617cc2e0b6dcd868b277089b731d7cefcb4a62fc0ddff7d7887108edf7043"
+      url "https://github.com/CommandZero/tless/releases/download/v0.1.2/tless-v0.1.2-x86_64-apple-darwin.tar.gz"
+      sha256 "eb819355d55edd57f133b50f05496c400eaaebf45c2ce963bb3275d2a9e90942"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/CommandZero/tless/releases/download/v0.1.1/tless-v0.1.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "492405e4d8eff4f4e37ce2c21f6ffa4e0c9265f8e9c28d5c857e9932b769ddea"
+      url "https://github.com/CommandZero/tless/releases/download/v0.1.2/tless-v0.1.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "e9cc942a18b2100fc6c9504179987be7f330b686313dd9e9ecdf44680dcc33ca"
     end
     on_intel do
-      url "https://github.com/CommandZero/tless/releases/download/v0.1.1/tless-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "968362e3b017c7165410f84b2dad9715677cb94a49e670997cde29852875ff50"
+      url "https://github.com/CommandZero/tless/releases/download/v0.1.2/tless-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "3353cf76aa2564fa1216c6b2156910cd45203c5cfa6744c6429937728ded3019"
     end
   end
 
